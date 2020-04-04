5e3193be38a6008b69afa9a8809869c913d6712c
Benitk <31191253+Benitk@users.noreply.github.com>
2020-04-04 21:45:08 +0300

    add node.h changes

--- Node.hpp
+++ Node.hpp
@@ -1,39 +1,37 @@
 #pragma once
 #include <iostream>
 
-
 class Node {
 
-    //attributes
+    //private data
     std::string name;
-    unsigned int level; 
+    unsigned int level;
+    char gender; 
     Node* left;
     Node* right;
+    Node* child;
 
     public:
     //getters
     std::string getName() { return this->name; }
     Node* getLeft() { return this->left; }
     Node* getRight() { return this->right; }
+    Node* getChild() { return this->child; }
+    char getGender() { return this->gender; }
     unsigned int getLevel() { return this->level; }
 
     //setters
-    void setLevel(unsigned int l){
-        this->level = l;
-    }
-    void setLeft(Node* n){
-        this->left = n;
-    }
-    void setRight(Node* n){
-        this->right = n;
-    }
+    void setLevel(unsigned int l){ this->level = l; }
+    void setLeft(Node* n){ this->left = n; }
+    void setRight(Node* n){ this->right = n; }
+    void setChild(Node* n){ this->child = n; }
+    void setGender(char g){ this->gender = g; }
 
     //constructor
-    Node(std::string name, unsigned int l = 0){
+    Node(std::string name, unsigned int l = 0) {
         this->name = name;
         this->level = l;
-        this->left = nullptr;
-        this->right = nullptr;
+        this->left = this->right = this->child = nullptr;
     }
     //destructor
     ~Node(){}
